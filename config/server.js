e3303553cb0a598d36994e61bc5160e586146303
dongdong <dongdong@dongdongui-MacBookPro.local>
2023-09-26 20:07:42 +0900

    test3

--- config/server.js
+++ config/server.js
@@ -2,7 +2,8 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("toBeModified1,toBeModified2"),
+    // keys: env.array("APP_KEYS"),
+    keys: ["CNQlc1LH5x5iuIvaQqtT6Q", "nFV87Stp88RV0NlLQYlyqA"], // 여기에 필요한 키를 설정합니다.
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
