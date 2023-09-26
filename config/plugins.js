0c536da756657e5cbf0dc23b2a2ac99b8b290457
dongdong <dongdong@dongdongui-MacBookPro.local>
2023-09-27 00:43:50 +0900

    test7

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,14 @@
 // config/plugins.js
 
 module.exports = ({ env }) => ({
-  "users-permissions": {
-    jwtSecret: "CC1EpvWL9T1Rg8cj13XBcA",
-    enabled: true,
-    config: {
-      jwt: {
-        expiresIn: "1m", //토큰 만료시간
+  plugins: {
+    "users-permissions": {
+      jwtSecret: process.env.JWT_SECRET || "yourSecretKeyHere",
+      enabled: true,
+      config: {
+        jwt: {
+          expiresIn: "1m",
+        },
       },
     },
   },
